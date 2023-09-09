c3cdeb584cddfd2def76c697b89aedf256ad76e4
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-09-09 15:15:43 -0500

    list in progress

--- 2.lists.py
+++ 2.lists.py
@@ -2,48 +2,80 @@
 
 # 1. Write a program that sum of all elements:
 one = [613, 955, 291, 497, 562, 483, 165, 210, 864, 789]
-
+sum = 0
+for n in one:
+    sum += n
+print(sum)
 # 2.  Write a program that find the largest element:
 two = [386, 850, 274, 316, 526, 937, 998, 249, 269, 922]
-
+highest = 0
+for n in two:
+    if n > highest:
+        highest = n
+print(highest)
 # 3. Write a program that duplicates that doubles the value of each elements in the list:
 # for example: [1,2,3] should result in [2,4,6]
 three = [211, 36, 295, 455, 147, 977, 381, 253, 327, 617]
-
+index = 0
+for n in three:
+    three[index] = n * 2
+    index += 1
+print(three)
 # 4. Write a program that concatenates these two list into a single list:
 four_a = [582, 427, 534, 143, 567, 604, 12, 48, 686, 825]
 four_b = [357, 728, 406, 989, 380, 800, 201, 410, 452, 141]
-
+print(four_a + four_b)
 
 # 5. Write a program that removes a specific element from a list by its value.
 five = [456, 942, 944, 762, 836, 451, 314, 559, 954, 211]
-
+def remove_by_value(value):
+    if value in five:
+        five.remove(value)
+remove_by_value(211)
+print(five)
 # 6. Write a program that removes a specific element from a list by its index.
 six = [993, 245, 896, 250, 226, 313, 918, 877, 793, 695]
-
+def remove_by_index(value):
+    six.remove(six[value])
+remove_by_index(9)
+print(six)
 # 7. Write a program that sorts a list of numbers in ascending order.
 seven = [887, 106, 368, 603, 35, 455, 728, 449, 108, 47]
-
-
+seven.sort()
+print(seven)
 # 8. Write a program that filters out all elements in a list that are less than a specified value.
 # use for loop and conditionals
 eight = [309, 122, 27, 240, 453, 174, 193, 649, 804, 171]
 threshold = 200
-
-
+new = filter(lambda n: n > threshold, eight)
+print("//// look here ////")
+print(list(new))
+print("//// look here ////")
 # 9. Calculate and print the length (number of elements) of a given list.
 nine = [679, 697, 657, 171, 503, 582, 656, 82, 724, 796]
-
-
+length = 0
+for n in nine:
+    length += 1
+print(length)
 # 10. Write a program that take a list and print a subset of its elements by specifying a start and end index.
 ten = [64, 800, 662, 185, 630, 612, 181, 210, 738, 12]
 start_index = 1
 end_index = 4
-
+ten = ten[start_index:end_index+1]
+print(ten)
 # 11. Write a program iterates the list and
-# prints 'FizzBuzz' when divisable by 3 and 5.  
-# prints 'Fizz' when divisable by 3 .  
+# prints 'FizzBuzz' when divisable by 3 and 5. 
+for n in ten:
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+# prints 'Fizz' when divisable by 3 .
+for n in ten:
+    if n % 3 == 0:
+        print("Fizz")  
 # prints 'Buzz' when divisable by 5. 
+for n in ten:
+    if n % 5 == 0:
+        print("Buzz")
 eleven = [213, 927, 265, 39, 860, 421, 550, 884, 991, 1500]
 
 
